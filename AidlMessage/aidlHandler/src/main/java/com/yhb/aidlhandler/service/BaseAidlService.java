5df6e2e2b8b69af54c3822b757d1df0dd11a2161
YeHaobo <17855366369@163.com>
2024-07-12 15:51:42 +0800

    移除V7包2024.07.12

--- AidlMessage/aidlHandler/src/main/java/com/yhb/aidlhandler/service/BaseAidlService.java
+++ AidlMessage/aidlHandler/src/main/java/com/yhb/aidlhandler/service/BaseAidlService.java
@@ -3,7 +3,6 @@ package com.yhb.aidlhandler.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
 /**继承该Base类实现你服务端的Service*/
@@ -16,7 +15,6 @@ public abstract class BaseAidlService extends Service implements ServiceAidlPost
     private ServiceAidlCall serverCall = new ServiceAidlCall(this);
 
     /**服务绑定*/
-    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return serverCall;
